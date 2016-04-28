c0f6f4964a9f877b433d4cc073739eac4a4152e2
meunicorn <yyf1994@hotmail.com>
2016-04-28 14:53:35 +0800

    把搜索框完美移到了右边，使用以下语句 searchview.setLayoutParams(new ActionBar.LayoutParams(Gravity.RIGHT));

--- app/src/main/java/com/meunicorn/fancy2u/UI/Activivty/MainActivity.java
+++ app/src/main/java/com/meunicorn/fancy2u/UI/Activivty/MainActivity.java
@@ -10,12 +10,17 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.meunicorn.fancy2u.Bean.Shots.Shot;
 import com.meunicorn.fancy2u.R;
@@ -75,9 +80,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 ActivityStart.toLogin(MainActivity.this);
             }
         });
+        SearchView searchView= (SearchView) findViewById(R.id.searchView);
+        searchView.setLayoutParams(new Toolbar.LayoutParams(Gravity.RIGHT));
+
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        viewPager= (ViewPager) findViewById(R.id.vp_main_display);
-        tabLayout= (TabLayout) findViewById(R.id.tab_main_title);
+        viewPager = (ViewPager) findViewById(R.id.vp_main_display);
+        tabLayout = (TabLayout) findViewById(R.id.tab_main_title);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
@@ -108,17 +116,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        MenuItem searchItem = (MenuItem) findViewById(R.id.search_shot);
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.search_shot:
+                ActivityStart.toSearchShot(this);
+                break;
+            case R.id.action_settings:
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT).show();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/meunicorn/fancy2u/Utils/ActivityStart.java
+++ app/src/main/java/com/meunicorn/fancy2u/Utils/ActivityStart.java
@@ -6,30 +6,37 @@ import android.os.Build;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.view.View;
+
 import com.meunicorn.fancy2u.Bean.Shots.Shot;
 import com.meunicorn.fancy2u.R;
 import com.meunicorn.fancy2u.UI.Activivty.DetailActivity;
 import com.meunicorn.fancy2u.UI.Activivty.LoginActivity;
+import com.meunicorn.fancy2u.UI.Activivty.SearchShotActivity;
 
 /**
  * Created by Fancy on 2016/3/30.
  */
 public class ActivityStart {
-    public static void toLogin(Activity activity){
+    public static void toLogin(Activity activity) {
         activity.startActivity(new Intent(activity, LoginActivity.class));
     }
-    public static void toDetail(Activity activity,Shot shot){
-        Intent intent=new Intent(activity,DetailActivity.class);
-        intent.putExtra("shot",shot);
-            View image= activity.findViewById(R.id.iv_shots_pic);
-            View title=activity.findViewById(R.id.tv_shots_title);
-            View author=activity.findViewById(R.id.tv_shots_designer);
+
+    public static void toDetail(Activity activity, Shot shot) {
+        Intent intent = new Intent(activity, DetailActivity.class);
+        intent.putExtra("shot", shot);
+        View image = activity.findViewById(R.id.iv_shots_pic);
+        View title = activity.findViewById(R.id.tv_shots_title);
+        View author = activity.findViewById(R.id.tv_shots_designer);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            activity.startActivity(intent,ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
-                    Pair.create(image,"transition_shot")
+            activity.startActivity(intent, ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
+                    Pair.create(image, "transition_shot")
 //                    Pair.create(title,"transition_title"),
 //                    Pair.create(author,"transition_designer")
             ).toBundle());
         }
+    }
+
+    public static void toSearchShot(Activity activity) {
+        activity.startActivity(new Intent(activity, SearchShotActivity.class));
     }
 }
